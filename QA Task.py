c8a0513a3f41a6020d61667f189708917108c57b
Lakis95 <66496002+Lakis95@users.noreply.github.com>
2020-06-08 14:38:03 +0100

    Add files via upload

---
+++ QA Task.py
@@ -0,0 +1,22 @@
+for i range (1, 101):
+    squared = i**2
+    if squared > 200
+        break
+
+number = int(input("Enter number:  ")):
+for i in range (1, number + 1)
+    squared = i**2
+    print(i, squared)
+
+
+
+
+
+
+
+
+
+
+
+
+
